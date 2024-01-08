f7a6b1612f6d63becd0b8dcb24871bbebe2afbd9
Fabian Villalobos <fabian.villalobos@alumnos.upm.es>
2024-01-08 22:23:16 +0100

    added footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import style from './page.module.css'
 import Header from './ui/Header';
+import Footer from './ui/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
---
+++ src/app/ui/Footer.js
@@ -0,0 +1,41 @@
+'use client'
+import React, { useState } from 'react'
+import styles from './Footer.module.css'
+import { Button, Container, FormHelperText, Input, InputLabel, Typography } from '@mui/material'
+import FormControl from '@mui/material/FormControl';
+
+export default function Footer() {
+  const [email, setEmail] = useState('');
+
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    // Add your logic here to handle the subscription (e.g., API request, state update, etc.)
+    console.log(`Subscribed with email: ${email}`);
+    // Clear the input field after subscription
+    setEmail('');
+  };
+
+  return (
+    <footer className={styles.footerMain}>
+      <div className={styles.footerContent}>
+        <div className={styles.newsletterForm}>
+          <h3 className={styles.header}>Stay Informed</h3>
+          <p>Subscribe to our newsletter for the latest news and updates.</p>
+
+          <form onSubmit={handleSubscribe}>
+            <Typography variant='body1' component="label" htmlFor='email' sx={{ marginRight: '10px' }}>We'll never share your email.</Typography>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <Button variant='contained' type="submit" size='small' sx={{ padding: '2px 8px', margin: '0 20px' }}>Subscribe</Button>
+          </form>
+        </div>
+      </div>
+    </footer>
+  )
+}
